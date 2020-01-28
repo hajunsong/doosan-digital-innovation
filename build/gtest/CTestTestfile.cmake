c05908742dd32699717cf0e54740aa28c20d88d7
hajunsong <hajunsong90@gmail.com>
2020-01-28 22:26:41 +0900

    key op 추가

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gmock
-# Build directory: /home/keti/Project/keti-doosan/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
